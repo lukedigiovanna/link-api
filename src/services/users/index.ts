6dc10f85278d473d56be6eb347ac3819b7de5931
lukedigiovanna <lukedigiovanna@gmail.com>
2022-05-15 01:08:31 -0400

    CoreUserService

--- src/services/users/index.ts
+++ src/services/users/index.ts
@@ -1,2 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Users } from "@prisma/client";
 
+class CoreUserService {
+    private prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    async getUsers(): Promise<Users[]> {
+        const users = await this.prisma.users.findMany({
+            // all
+        });
+
+        return users;
+    }
+}
+
+export default new CoreUserService();
